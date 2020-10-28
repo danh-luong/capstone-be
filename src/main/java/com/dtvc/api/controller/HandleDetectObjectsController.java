487d3612ccfca8776dbc532eb0a7e491494bed6e
chiennd <chientaolauca@gmail.com>
2020-10-28 14:22:39 +0700

    change name of function

--- src/main/java/com/dtvc/api/controller/HandleDetectObjectsController.java
+++ src/main/java/com/dtvc/api/controller/HandleDetectObjectsController.java
@@ -49,7 +49,7 @@ public class HandleDetectObjectsController {
     private PersonAndMotorbikeService personAndMotorbikeService;
 
     @PostMapping("/object")
-    public String callDetectObjectsApi(@RequestBody Map<String, String> imageBase64) {
+    public String callDetectObjectsApiV2(@RequestBody Map<String, String> imageBase64) {
         long start = System.currentTimeMillis();
         String listObject = null;
         String result = "";
@@ -140,94 +140,6 @@ public class HandleDetectObjectsController {
         System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
         return result;
     }
-
-//    }@PostMapping("/object")
-//    public String callDetectObjectsApi(@RequestBody Map<String, String> imageBase64) {
-//        long start = System.currentTimeMillis();
-//        CompletableFuture<String> helmetLocationListString = null;
-//        CompletableFuture<String> personAndMotorbikeListString = null;
-//        String result = "";
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
-//            //call async
-//            helmetLocationListString = helmetService.getListHelmetLocation(imageBase64);
-//            personAndMotorbikeListString = personAndMotorbikeService.getListPersonAndMotorbikeLocation(imageBase64);
-//            CompletableFuture.allOf(personAndMotorbikeListString, helmetLocationListString).join();
-//
-//            //parse Json to Object
-//            List<HelmetLocation> helmetLocationList = objectMapper.readValue(helmetLocationListString.get(), new TypeReference<List<HelmetLocation>>() {
-//            });
-//            Map<String, Object> jsonMap = objectMapper.readValue(personAndMotorbikeListString.get(),
-//                    new TypeReference<Map<String, Object>>() {
-//                    });
-//            Iterator jsonMapIterator = jsonMap.entrySet().iterator();
-//            List<PersonLocation> personLocationList = new ArrayList<>();
-//            List<MotorbikeLocation> motorbikeLocationList = new ArrayList<>();
-//            List<TrafficLight> lights = new ArrayList<>();
-//            while (jsonMapIterator.hasNext()) {
-//                Map.Entry pair = (Map.Entry) jsonMapIterator.next();
-//                if (String.valueOf(pair.getKey()).contains("person")) {
-//                    personLocationList.add(objectMapper.convertValue(pair.getValue(), PersonLocation.class));
-//                } else if (String.valueOf(pair.getKey()).contains("motorbike")) {
-//                    motorbikeLocationList.add(objectMapper.convertValue(pair.getValue(), MotorbikeLocation.class));
-//                } else if (String.valueOf(pair.getKey()).contains("traffic-light")) {
-//                    lights.add(objectMapper.convertValue(pair.getValue(), TrafficLight.class));
-//                }
-//            }
-//            boolean isRed = trafficLightService.isRed(lights);
-//            if (isRed) {
-////              The position of crossing line
-////              [x1, x2, y1, y2] line
-//                int[] line = {462, 1000, 468, 450};
-//                int[] lane = {462, 1000, 200, 230};
-////                calculate distance from line to lane
-//                int laneDistance = laneService.calculateDistance(line, lane);
-//                System.out.println("lane: " + laneDistance);
-//                int count = motorbikeService.detectPassingRedLight(motorbikeLocationList, line, Constants.LANE_RATE, laneDistance);
-//                if (count > 0) {
-//                    result = "There are " + count + " cases of red light violation";
-//                }
-//                //Match PersonLocation and MotorbikeLocation
-//                Map<PersonLocation, MotorbikeLocation> personLocationMotorbikeLocationMap = new WearingHelmetAlgorithm().isRelatedPersonAndMotorbike(motorbikeLocationList, personLocationList);
-//
-//                //Get all PersonLocation base on Motorbike
-//                List<PersonLocation> matchedPersonMotorbike = new ArrayList<PersonLocation>(personLocationMotorbikeLocationMap.keySet());
-//
-//                //Match map HelmetLocation and MotorbikeLocation with HelmetLocation
-//                Map<PersonLocation, HelmetLocation> matchedPersonWithHelmetMap = new WearingHelmetAlgorithm().isRelatedPersonAndHelmet(matchedPersonMotorbike, helmetLocationList);
-//
-//                //Get List PersonLocation and MotorbikeLocation base on HelmetLocation
-//                List<PersonLocation> matchedPersonHelmetList = new ArrayList<PersonLocation>(matchedPersonWithHelmetMap.keySet());
-//
-//                //Check isWearingHelmet or Not
-//                Map<PersonLocation, MotorbikeLocation> notWearingHelmet = new HashMap<>();
-//                int countNotHelmet = 0;
-//                if (matchedPersonMotorbike.size() != matchedPersonHelmetList.size()) {
-//                    for (int i = 0; i < matchedPersonMotorbike.size(); i++) {
-//                        if (!matchedPersonWithHelmetMap.containsKey(matchedPersonMotorbike.get(i))) {
-//                            notWearingHelmet.put(matchedPersonMotorbike.get(i),
-//                                    personLocationMotorbikeLocationMap.get(matchedPersonMotorbike.get(i)));
-//                            countNotHelmet++;
-//                            MotorbikeLocation motorbike = personLocationMotorbikeLocationMap.get(matchedPersonMotorbike.get(i));
-//                            System.out.println("left: " + motorbike.getLeft() + " | right: " + motorbike.getRight()
-//                                    + " | top: " + motorbike.getTop() + " | bottom: " + motorbike.getBottom());
-//                        }
-//                    }
-//                }
-//                if (countNotHelmet > 0) {
-//                    result += "\nThere are " + countNotHelmet + " cases of violation without wearing a helmet";
-//                }
-//            }
-//            if (result.isEmpty()) {
-//                result = "Not violation";
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println("Elapsed time: " + (System.currentTimeMillis() - start));
-//        return result;
-//    }
 
 
 }
